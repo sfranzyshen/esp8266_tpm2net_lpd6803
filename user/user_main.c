4ad9d403df34a62d39c3151433376479f0ffb067
Scott Franzyshen <sfranzyshen@gmail.com>
2015-02-18 14:25:31 -0800

    Update user_main.c

--- user/user_main.c
+++ user/user_main.c
@@ -1,4 +1,4 @@
-#include "ws2812.h"
+#include "lpd6803.h"
 #include "tpm2net.h"
 
 void user_init(void)
@@ -7,7 +7,7 @@ void user_init(void)
 	wifi_station_dhcpc_start();
 
 	tpm2net_init();
-	ws2812_init();
+	lpd6803_init();
 }
 
 
